b8d9ada976ec07208f1fdb40947ec8324faf9420
Ruslan Gramatic <ruslan.gramatic@opower.com>
2019-01-20 17:51:15 +0200

    Fixed integration controller tests

---
+++ src/test/java/ua/od/game/controller/ControllerTest.java
@@ -0,0 +1,36 @@
+package ua.od.game.controller;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.junit.After;
+import org.junit.Before;
+
+public class ControllerTest {
+    private Server server;
+
+    @Before
+    public void startJetty() throws Exception {
+        // Create Server
+        server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler();
+        ServletHolder defaultServ = new ServletHolder("default", DefaultServlet.class);
+        defaultServ.setInitParameter("resourceBase",System.getProperty("user.dir"));
+        defaultServ.setInitParameter("dirAllowed","true");
+        context.addServlet(defaultServ,"/");
+        server.setHandler(context);
+
+        // Start Server
+        server.start();
+    }
+
+    @After
+    public void stopJetty() {
+        try {
+            server.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/ua/od/game/controller/impl/RoomControllerImplTest.java
@@ -0,0 +1,23 @@
+package ua.od.game.controller.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.od.game.controller.ControllerTest;
+
+import java.io.IOException;
+
+public class RoomControllerImplTest extends ControllerTest {
+
+    RoomControllerImpl roomControllerImpl;
+
+    @Before
+    public void init() {
+        roomControllerImpl = new RoomControllerImpl();
+    }
+
+    @Test
+    public void getAllRoomListTest() throws IOException {
+
+    }
+}
+
