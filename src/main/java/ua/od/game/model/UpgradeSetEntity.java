e65d3c74064f97fae041a01993a51e4dc5aefbea
Temrad <artemiy.radiyk@gmail.com>
2019-02-04 17:18:52 +0200

    getters, setters, toString in UpgradeSetEntity

--- src/main/java/ua/od/game/model/UpgradeSetEntity.java
+++ src/main/java/ua/od/game/model/UpgradeSetEntity.java
@@ -5,4 +5,47 @@ public class UpgradeSetEntity {
     Integer setId;
     Integer upgradeId;
     Float amount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSetId() {
+        return setId;
+    }
+
+    public void setSetId(Integer setId) {
+        this.setId = setId;
+    }
+
+    public Integer getUpgradeId() {
+        return upgradeId;
+    }
+
+    public void setUpgradeId(Integer upgradeId) {
+        this.upgradeId = upgradeId;
+    }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "UpgradeSetEntity{" +
+                "id=" + id +
+                ", setId=" + setId +
+                ", upgradeId=" + upgradeId +
+                ", amount=" + amount +
+                '}';
+    }
+
 }
