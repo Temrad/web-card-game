efb5271d8a6f2e7c67844047843131af0ff99be2
Temrad <artemiy.radiyk@gmail.com>
2019-02-08 23:18:29 +0200

    finished AccountNotificationDao

--- src/main/java/ua/od/game/repository/dao/impl/AccountNotificationDaoImpl.java
+++ src/main/java/ua/od/game/repository/dao/impl/AccountNotificationDaoImpl.java
@@ -1,8 +1,11 @@
 package ua.od.game.repository.dao.impl;
 
-import ua.od.game.model.NotificationEntity;
+import ua.od.game.model.AccountNotificationEntity;
 import ua.od.game.repository.dao.AccountNotificationDao;
+import ua.od.game.repository.helper.SqlHelper;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AccountNotificationDaoImpl implements AccountNotificationDao {
@@ -15,7 +18,7 @@ public class AccountNotificationDaoImpl implements AccountNotificationDao {
             "        select notification.id from notification\n" +
             "          inner join RESOURCE_SET\n" +
             "            on NOTIFICATION.RESOURCE_SET_ID = RESOURCE_SET.SET_ID\n" +
-            "          inner join (select * from ACCOUNT_RESOURCE where ACCOUNT_ID = 2) as accResources\n" +
+            "          inner join (select * from ACCOUNT_RESOURCE where ACCOUNT_ID = ?) as accResources\n" +
             "            on RESOURCE_SET.RESOURCE_ID = accResources.RESOURCE_ID\n" +
             "            and RESOURCE_SET.AMOUNT > 0\n" +
             "            and RESOURCE_SET.AMOUNT <= accResources.AMOUNT\n" +
@@ -29,7 +32,7 @@ public class AccountNotificationDaoImpl implements AccountNotificationDao {
             "              select notification.id from notification\n" +
             "                  inner join BUILDING_SET\n" +
             "                    on NOTIFICATION.BUILDING_SET_ID = BUILDING_SET.SET_ID\n" +
-            "                  left join (select * from ACCOUNT_BUILDING where ACCOUNT_ID = 2) as accBuildings\n" +
+            "                  left join (select * from ACCOUNT_BUILDING where ACCOUNT_ID = ?) as accBuildings\n" +
             "                    on BUILDING_SET.BUILDING_ID = accBuildings.BUILDING_ID\n" +
             "                    and BUILDING_SET.AMOUNT <= accBuildings.AMOUNT\n" +
             "              where accBuildings.ID is null))\n" +
@@ -42,23 +45,40 @@ public class AccountNotificationDaoImpl implements AccountNotificationDao {
             "             select notification.id from notification\n" +
             "                  inner join UPGRADE_SET\n" +
             "                     on NOTIFICATION.UPGRADE_SET_ID = UPGRADE_SET.SET_ID\n" +
-            "                  left join (select * from ACCOUNT_UPGRADE where ACCOUNT_ID = 2) as accUpgrades\n" +
+            "                  left join (select * from ACCOUNT_UPGRADE where ACCOUNT_ID = ?) as accUpgrades\n" +
             "                     on UPGRADE_SET.UPGRADE_ID = accUpgrades.UPGRADE_ID\n" +
             "                     and UPGRADE_SET.AMOUNT <= accUpgrades.AMOUNT\n" +
             "             where accUpgrades.ID is null))\n" +
             "    ) as DeservedNotifications\n" +
             "\n" +
-            "    left join (select * from ACCOUNT_NOTIFICATION where ACCOUNT_ID = 2) as accNotifications\n" +
+            "    left join (select * from ACCOUNT_NOTIFICATION where ACCOUNT_ID = ?) as accNotifications\n" +
             "                   on DeservedNotifications.ID = accNotifications.NOTIFICATION_ID\n" +
             "    where accNotifications.ID is null;\n";
+    private static String CLEAR_ACC_NOTIFICATIONS = "delete from ACCOUNT_NOTIFICATION where ACCOUNT_ID = ?;";
 
     @Override
     public Boolean clearAccountNotificationList(Integer accountId) {
-        return null;
+        int affectedRows =
+        SqlHelper.prepareStatement(CLEAR_ACC_NOTIFICATIONS, statement -> {
+            statement.setInt(1, accountId);
+            return statement.executeUpdate();
+        });
+        return affectedRows > 0;
     }
 
     @Override
-    public List<NotificationEntity> getAccountRecentNotificationList(Integer accountId) {
-        return null;
+    public List<AccountNotificationEntity> getAccountRecentNotificationList(Integer accountId) {
+        List<AccountNotificationEntity> notificationList = new ArrayList<>();
+        SqlHelper.prepareStatement(GET_ACCOUNT_RECENT_NOTIFICATIONS, statement -> {
+            for(int paramIndex = 1; paramIndex <= 4; paramIndex++)
+                statement.setInt(paramIndex, accountId);
+            ResultSet resultSet = statement.executeQuery();
+            while( resultSet.next() )
+                notificationList.add(new AccountNotificationEntity() {{
+                    setNotificationId(resultSet.getInt(1));
+                }});
+            return null;
+        });
+       return notificationList;
     }
 }
