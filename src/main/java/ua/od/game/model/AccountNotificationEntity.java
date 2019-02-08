5a49eb9624c32e0cb0b55f1747dda0dc7389c59d
Temrad <artemiy.radiyk@gmail.com>
2019-02-08 23:17:16 +0200

    Various minor changes

--- src/main/java/ua/od/game/model/AccountNotificationEntity.java
+++ src/main/java/ua/od/game/model/AccountNotificationEntity.java
@@ -2,4 +2,19 @@ package ua.od.game.model;
 
 public class AccountNotificationEntity {
     private Integer notificationId;
+
+    public Integer getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(Integer notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    @Override
+    public String toString() {
+        return "\nAccountNotificationEntity{" +
+                "notificationId=" + notificationId +
+                '}';
+    }
 }
--- src/main/java/ua/od/game/repository/dao/AccountNotificationDao.java
+++ src/main/java/ua/od/game/repository/dao/AccountNotificationDao.java
@@ -1,10 +1,11 @@
 package ua.od.game.repository.dao;
 
+import ua.od.game.model.AccountNotificationEntity;
 import ua.od.game.model.NotificationEntity;
 
 import java.util.List;
 
 public interface AccountNotificationDao {
-    Boolean clearUserNotificationList(Integer accountId);
-    List<NotificationEntity> getUserRecentNotificationList(Integer accountId);
+    Boolean clearAccountNotificationList(Integer accountId);
+    List<AccountNotificationEntity> getAccountRecentNotificationList(Integer accountId);
 }
