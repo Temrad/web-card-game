33f87aa4460bc80d2784cd2f3b6739450efd8c07
Temrad <artemiy.radiyk@gmail.com>
2019-02-12 16:51:25 +0200

    Better test for AccountNotificationDao

--- src/test/java/ua/od/game/repository/dao/impl/AccountNotificationDaoImplTest.java
+++ src/test/java/ua/od/game/repository/dao/impl/AccountNotificationDaoImplTest.java
@@ -7,21 +7,32 @@ import ua.od.game.repository.dao.DbTest;
 import java.util.List;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class AccountNotificationDaoImplTest extends DbTest {
     private AccountNotificationDaoImpl accountNotificationDao = new AccountNotificationDaoImpl();
+    private final Integer accountId = 1;
 
     @Test
     public void getAccountRecentNotificationListTest() {
         List<AccountNotificationEntity> accountNotificationEntities;
-        accountNotificationEntities = accountNotificationDao.getAccountRecentNotificationList(1);
+        accountNotificationEntities = accountNotificationDao.getAccountRecentNotificationList(accountId);
+        // must return at least 'show-from-scratch' notifications:
+        assertFalse(accountNotificationEntities.isEmpty());
         System.out.println(accountNotificationEntities);
+        // must return nothing - account_notification has been updated in prev call
+        assertTrue(accountNotificationDao.getAccountRecentNotificationList(accountId).isEmpty());
+        // removes all from account_notification
+        clearAccountNotificationListTest();
+        // must return at least 'show-from-scratch' notifications:
+        assertFalse(accountNotificationDao.getAccountRecentNotificationList(accountId).isEmpty());
     }
 
     @Test
     public void clearAccountNotificationListTest() {
-        boolean affected = accountNotificationDao.clearAccountNotificationList(1);
+        boolean affected = accountNotificationDao.clearAccountNotificationList(accountId);
         System.out.println("affected:" + affected);
-        assertFalse(accountNotificationDao.clearAccountNotificationList(1));
+        // second call has nothing to clear, always returns false:
+        assertFalse(accountNotificationDao.clearAccountNotificationList(accountId));
     }
 }
--- src/test/resources/db/07_Notifications.sql
+++ src/test/resources/db/07_Notifications.sql
@@ -4,6 +4,6 @@ VALUES
        ('Hello, brave new world', 'So you are basically a hobo now, eh?. Seems like you`ll have to start your life from scratch!', 1, null, null, null ),
        ('How to survive?', 'Survival is crucial, you know, so its worth considering. Go build something. Help yourself.', 1, null, null, null),
        ('Nice!', 'Now, when you can make booze, you are on your way to making first friends!', 0, 4, null, null),
-			 ('Test','for testing purposes, delete later', 0, 11, 1, null),
+			 ('Test','delete later', 0, 11, 1, null),
 			 ('testAccountNotif', 'delete later', 0, 4, 21, null)
 ;
