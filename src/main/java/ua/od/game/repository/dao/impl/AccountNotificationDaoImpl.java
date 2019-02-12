5885392043e81b1da9d41cd85b6c59e30c34f81d
Temrad <artemiy.radiyk@gmail.com>
2019-02-12 16:50:13 +0200

    AccountNotificationDao updates Account_Notification

--- src/main/java/ua/od/game/repository/dao/impl/AccountNotificationDaoImpl.java
+++ src/main/java/ua/od/game/repository/dao/impl/AccountNotificationDaoImpl.java
@@ -11,50 +11,51 @@ import java.util.List;
 public class AccountNotificationDaoImpl implements AccountNotificationDao {
     private static String GET_ACCOUNT_RECENT_NOTIFICATIONS =
             "select DeservedNotifications.ID from (\n" +
-            "\n" +
+            "  -- get notifications deserved according to account`s resources & buildings & upgrades\n" +
+            "  select notification.id from notification\n" +
+            "  where notification.id IN (\n" +
+            "    -- get notifications which the account deserves according to it`s resources:\n" +
             "    select notification.id from notification\n" +
-            "    where notification.id IN (\n" +
+            "      inner join RESOURCE_SET\n" +
+            "        on NOTIFICATION.RESOURCE_SET_ID = RESOURCE_SET.SET_ID\n" +
+            "      inner join (select * from ACCOUNT_RESOURCE where ACCOUNT_ID = ?) as accResorces\n" +
+            "        on RESOURCE_SET.RESOURCE_ID = accResorces.RESOURCE_ID\n" +
+            "        and RESOURCE_SET.AMOUNT > 0\n" +
+            "        and RESOURCE_SET.AMOUNT <= accResorces.AMOUNT\n" +
+            "    union (select notification.id from NOTIFICATION where RESOURCE_SET_ID is null))\n" +
             "\n" +
-            "        select notification.id from notification\n" +
-            "          inner join RESOURCE_SET\n" +
-            "            on NOTIFICATION.RESOURCE_SET_ID = RESOURCE_SET.SET_ID\n" +
-            "          inner join (select * from ACCOUNT_RESOURCE where ACCOUNT_ID = ?) as accResources\n" +
-            "            on RESOURCE_SET.RESOURCE_ID = accResources.RESOURCE_ID\n" +
-            "            and RESOURCE_SET.AMOUNT > 0\n" +
-            "            and RESOURCE_SET.AMOUNT <= accResources.AMOUNT\n" +
-            "          union (select notification.id from NOTIFICATION where RESOURCE_SET_ID is null))\n" +
+            "  and notification.id IN (\n" +
+            "    -- get notifications which the account deserves according to it`s buildings:\n" +
+            "    select notification.id from NOTIFICATION\n" +
+            "    where notification.id NOT IN(\n" +
+            "      -- get notifications which are NOT deserved according to account`s buildings:\n" +
+            "      select notification.id from notification\n" +
+            "        inner join BUILDING_SET\n" +
+            "          on NOTIFICATION.BUILDING_SET_ID = BUILDING_SET.SET_ID\n" +
+            "        left join (select * from ACCOUNT_BUILDING where ACCOUNT_ID = ?) as accBuildings\n" +
+            "          on BUILDING_SET.BUILDING_ID = accBuildings.BUILDING_ID                                                                                                                          and BUILDING_SET.AMOUNT <= accBuildings.AMOUNT\n" +
+            "      where accBuildings.ID is null))\n" +
             "\n" +
-            "    and notification.id IN (\n" +
-            "\n" +
-            "        select notification.id from NOTIFICATION\n" +
-            "        where notification.id NOT IN(\n" +
-            "\n" +
-            "              select notification.id from notification\n" +
-            "                  inner join BUILDING_SET\n" +
-            "                    on NOTIFICATION.BUILDING_SET_ID = BUILDING_SET.SET_ID\n" +
-            "                  left join (select * from ACCOUNT_BUILDING where ACCOUNT_ID = ?) as accBuildings\n" +
-            "                    on BUILDING_SET.BUILDING_ID = accBuildings.BUILDING_ID\n" +
-            "                    and BUILDING_SET.AMOUNT <= accBuildings.AMOUNT\n" +
-            "              where accBuildings.ID is null))\n" +
-            "\n" +
-            "    and notification.id IN (\n" +
-            "\n" +
-            "        select notification.id from NOTIFICATION\n" +
-            "        where notification.id NOT IN(\n" +
-            "\n" +
-            "             select notification.id from notification\n" +
-            "                  inner join UPGRADE_SET\n" +
-            "                     on NOTIFICATION.UPGRADE_SET_ID = UPGRADE_SET.SET_ID\n" +
-            "                  left join (select * from ACCOUNT_UPGRADE where ACCOUNT_ID = ?) as accUpgrades\n" +
-            "                     on UPGRADE_SET.UPGRADE_ID = accUpgrades.UPGRADE_ID\n" +
-            "                     and UPGRADE_SET.AMOUNT <= accUpgrades.AMOUNT\n" +
-            "             where accUpgrades.ID is null))\n" +
-            "    ) as DeservedNotifications\n" +
-            "\n" +
-            "    left join (select * from ACCOUNT_NOTIFICATION where ACCOUNT_ID = ?) as accNotifications\n" +
-            "                   on DeservedNotifications.ID = accNotifications.NOTIFICATION_ID\n" +
-            "    where accNotifications.ID is null;\n";
+            "  and notification.id IN (\n" +
+            "    -- get notifications which the account deserves according to it`s upgrades:\n" +
+            "    select notification.id from NOTIFICATION\n" +
+            "    where notification.id NOT IN(\n" +
+            "      -- get notifications which are NOT deserved according to account`s upgrades:\n" +
+            "      select notification.id from notification\n" +
+            "        inner join UPGRADE_SET\n" +
+            "          on NOTIFICATION.UPGRADE_SET_ID = UPGRADE_SET.SET_ID\n" +
+            "        left join (select * from ACCOUNT_UPGRADE where ACCOUNT_ID = ?) as accUpgrades\n" +
+            "          on UPGRADE_SET.UPGRADE_ID = accUpgrades.UPGRADE_ID                                                                                                                              and UPGRADE_SET.AMOUNT <= accUpgrades.AMOUNT\n" +
+            "      where accUpgrades.ID is null))\n" +
+            ") as DeservedNotifications\n" +
+            "  -- get rid of notifications which have been shown to this acc already:\n" +
+            "  left join (select * from ACCOUNT_NOTIFICATION where ACCOUNT_ID = ?) as accNotifications\n" +
+            "    on DeservedNotifications.ID = accNotifications.NOTIFICATION_ID\n" +
+            "where accNotifications.ID is null;";
+
     private static String CLEAR_ACC_NOTIFICATIONS = "delete from ACCOUNT_NOTIFICATION where ACCOUNT_ID = ?;";
+    private static String UPDATE_ACC_NOTIFICATIONS =
+            "insert into ACCOUNT_NOTIFICATION (ACCOUNT_ID, NOTIFICATION_ID) values (?,?);";
 
     @Override
     public Boolean clearAccountNotificationList(Integer accountId) {
@@ -79,6 +80,18 @@ public class AccountNotificationDaoImpl implements AccountNotificationDao {
                 }});
             return null;
         });
-       return notificationList;
+        updateAccountNotification(notificationList, accountId);
+        return notificationList;
+    }
+
+    private void updateAccountNotification(List<AccountNotificationEntity> notificationList, Integer accountId) {
+        SqlHelper.prepareStatement(UPDATE_ACC_NOTIFICATIONS, statement -> {
+            statement.setInt(1, accountId);
+            for (AccountNotificationEntity notification : notificationList) {
+                statement.setInt(2, notification.getNotificationId());
+                statement.executeUpdate();
+            }
+            return null;
+        });
     }
 }
